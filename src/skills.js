59555bfc3b8db9dd13c0894fc3e6dbced816d08a
blitty-codes <blittynet@gmail.com>
2021-08-23 14:57:02 +0200

    changed skills to tech_skills

--- src/skills.js
+++ src/skills.js
@@ -2,59 +2,51 @@ const Skills = {
 	data() {
 		return {
 			loading: true,
-			skills: [],
+			tech_skills: [],
 		}
 	},
 	mounted() {
 		this.loading = false
-		this.skills = [
+		this.tech_skills = [
 			{
 				id: 1,
 				text: 'HTML',
 				css: 'html',
-				percentaje: '90%',
 			},
 			{
 				id: 2,
 				text: 'CSS',
 				css: 'css',
-				percentaje: '78%',
 			},
 			{
 				id: 3,
 				text: 'JS',
 				css: 'js',
-				percentaje: '80%',
 			},
 			{
 				id: 4,
 				text: 'TS',
 				css: 'ts',
-				percentaje: '40%',
 			},
 			{
 				id: 5,
 				text: 'C++',
 				css: 'cpp',
-				percentaje: '63%',
 			},
 			{
 				id: 6,
 				text: 'C',
 				css: 'c',
-				percentaje: '70%',
 			},
 			{
 				id: 7,
-				text: 'Py',
+				text: 'Python',
 				css: 'python',
-				percentaje: '40%',
 			},
 			{
 				id: 8,
 				text: 'React',
 				css: 'react',
-				percentaje: '68%',
 			},
 		]
 	},
